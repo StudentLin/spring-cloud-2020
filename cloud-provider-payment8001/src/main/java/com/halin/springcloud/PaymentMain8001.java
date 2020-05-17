aaafd7bf12f3719da567cf47670060f08c731672
StudentLin <346454850@qq.com>
2020-05-17 22:52:35 +0800

    init project

---
+++ cloud-provider-payment8001/src/main/java/com/halin/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.halin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @AUTHOR HAN LIN
+ * @DATE 2020/5/17 周日
+ */
+
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
